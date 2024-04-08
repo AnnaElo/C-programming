e49429bb9d6ba0a3320997380beaf05df2071893
Your Name <you@example.com>
2024-04-08 15:52:31 +0300

    Part Two done

---
+++ Exercise1/Part_Two.c
@@ -0,0 +1,23 @@
+// Write a program that prompts the user to input their age. If the age is less than 13, 
+// print "Child", if it's between 13 and 19, print "Teenager", otherwise print "Adult". 
+// Ensure to handle input securely.
+
+#include <stdio.h>
+
+int main(void) {
+    int age; 
+
+   printf("Please, input your age\n");
+   scanf("%d", &age);
+
+    if (age < 13) {
+        printf("Child");}
+    
+    if (age >= 13 && age <= 19) {
+        printf("Teenager");}
+
+    else; {
+        printf("Adult");}
+
+   return 0;
+}
