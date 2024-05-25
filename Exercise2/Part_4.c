81a46711f07bf13a2421b66c4d0935663818e5c2
Your Name <you@example.com>
2024-05-25 17:03:24 +0300

    Part 4 done

---
+++ Exercise2/Part_4.c
@@ -0,0 +1,69 @@
+// Use an enum to represent the days of the week. Write a program that converts 
+// a number input by the user to the corresponding day of the week and prints it.
+
+#include <stdio.h>
+
+// Define an enum type for the days of the week
+enum DaysOfWeek {
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
+};
+
+int main() {
+    int dayNumber;
+
+    while (1) {
+        // Prompt the user to input a number between 1 and 7
+        printf("Enter a number between 1 and 7: ");
+        if (scanf("%d", &dayNumber) != 1) {
+            printf("Invalid input. Please enter a valid number.\n");
+            // Clear input buffer
+            while (getchar() != '\n');
+            continue;
+        }
+
+        // Check if input is within valid range
+        if (dayNumber < 1 || dayNumber > 7) {
+            printf("Input out of range. Please enter a number between 1 and 7.\n");
+            continue;
+        }
+
+        // Use a switch-case statement to print the corresponding day of the week
+        switch (dayNumber) {
+            case Sunday:
+                printf("Sunday\n");
+                break;
+            case Monday:
+                printf("Monday\n");
+                break;
+            case Tuesday:
+                printf("Tuesday\n");
+                break;
+            case Wednesday:
+                printf("Wednesday\n");
+                break;
+            case Thursday:
+                printf("Thursday\n");
+                break;
+            case Friday:
+                printf("Friday\n");
+                break;
+            case Saturday:
+                printf("Saturday\n");
+                break;
+            default:
+                // This should not be reached, but added for completeness
+                printf("Unknown error occurred.\n");
+                break;
+        }
+
+        break; // Exit the loop after valid input
+    }
+
+    return 0;
+}
